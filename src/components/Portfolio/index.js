ee26de8f53cd50f6bc7cc01d0025b8139ab20db1
Adam Field <adamjfield@gmail.com>
2022-02-11 21:39:38 -0600

    added map to portfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Portfolio() {
   const techLogos = [
@@ -12,6 +12,27 @@ function Portfolio() {
     'mongoose',
     'react',
   ];
+
+  const [projects] = useState([
+    {
+      name: 'Travel Blog',
+      file: 'travel-blog',
+      description: '',
+      tech: ['Handlebars, ', 'MySQL, ', 'Sequelize, ', 'Cloudinary API'],
+    },
+    {
+      name: 'Weather Dashboard',
+      file: 'weather-dashboard',
+      description: '',
+      tech: ['Bootstrap, ', 'OpenWeather API'],
+    },
+    {
+      name: 'Dinner and a Movie',
+      file: 'dinner-movie',
+      description: '',
+      tech: ['Materialize CSS, ', 'Edamam Recipe API, ', 'The Movie Database API'],
+    },
+  ]);
 
   return (
     <section>
@@ -30,45 +51,23 @@ function Portfolio() {
       </div>
       <h2>My Work</h2>
       <div className='projects-div'>
-        <div className='project-tile'>
-          <div className='project-title'>
-            <h3>Project Name</h3>
-            <h6>Project Tech Used</h6>
-          </div>
-          <img
-            className='project-img'
-            src={require('../../assets/images/travel-blog.png')}
-          />
+        {projects.map((project, i) => (
+          <div className='project-tile'>
+            <div className='project-title'>
+              <h3>{project.name}</h3>
+              <h6>{project.tech}</h6>
+            </div>
+            <img
+              className='project-img'
+              src={require(`../../assets/images/${project.file}.png`)}
+              alt={project.file}
+              key={project.name}
+            />
             <button className='btn btn-lg btn-outline-danger tile-btn'>
               Learn More
             </button>
-        </div>
-        <div className='project-tile'>
-          <div className='project-title'>
-            <h3>Project Name</h3>
-            <h6>Project Tech Used</h6>
           </div>
-          <img
-            className='project-img'
-            src={require('../../assets/images/weather-dashboard.png')}
-          />
-            <button className='btn btn-lg btn-outline-danger tile-btn'>
-              Learn More
-            </button>
-        </div>
-        <div className='project-tile'>
-          <div className='project-title'>
-            <h3>Project Name</h3>
-            <h6>Project Tech Used</h6>
-          </div>
-          <img
-            className='project-img'
-            src={require('../../assets/images/dinner-movie.png')}
-          />
-            <button className='btn btn-lg btn-outline-danger tile-btn'>
-              Learn More
-            </button>
-        </div>
+        ))}
       </div>
     </section>
   );
